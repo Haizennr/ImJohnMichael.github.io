fd9a72a04323b95953953b804115ff28f6622dea
Haizennr <153291030+Haizennr@users.noreply.github.com>
2023-12-16 21:30:13 +0800

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+
+const spans = document.querySelectorAll('.progress-bar span');
+
+spans.forEach((span) => {
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+});
+
+
+  
